52933fb81a73c92f203a0c2c18e3303dee7d3ca5
Afiz Savage <savageafiz9@gmail.com>
2021-12-23 15:15:33 +0000

    Setup api config

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,1 +1,27 @@
 import '../styles/index.css';
+
+const fetchScores = async () => {
+  const response = await fetch(
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/C1YVwF2qfkI9ZCq4djAP/scores'
+  );
+  const scores = await response.json();
+  return scores;
+};
+
+const displayScores = (scoresArg) => {
+  const listContainer = document.querySelector('ul');
+
+  let listItems = '';
+  scoresArg.result.forEach((score) => {
+    listItems += `<li>${score.user}: ${score.score}</li>`;
+    listContainer.innerHTML = listItems;
+  });
+};
+
+const load = () => {
+  fetchScores().then((scores) => {
+    displayScores(scores);
+  });
+};
+
+window.onload = load;
